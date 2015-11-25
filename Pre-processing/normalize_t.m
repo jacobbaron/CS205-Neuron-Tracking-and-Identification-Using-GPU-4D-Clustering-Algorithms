40034f39c55ea56d770ff9a7c5efa95ad4d134fe
jacobbaron <jbaron@physics.harvard.edu>
2015-11-25 11:40:01 -0500

    Getting rid of old files

--- Pre-processing/normalize_t.m
+++
@@ -1,8 +0,0 @@
-function [nm_img_stack]=normalize_t(img_stack,n)
-    close all;
-    sz=size(img_stack);
-    l=sqrt(sum(img_stack.^2,4));
-    lrep=repmat(l,1,1,1,sz(4));
-    nm_img_stack=img_stack./(lrep+n);
-
-end
